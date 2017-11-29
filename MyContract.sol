91579c2617087b2fe41111e9458a6bfac5517a3f
Vadim <cojocaru.vadim@gmail.com>
2017-11-29 14:18:08 +0200

    first contract

--- MyContract.sol
+++ MyContract.sol
@@ -2,6 +2,14 @@ pragma solidity ^0.4.18;
 
 contract MyContract {
   function MyContract() public {
+    uint storedData;
 
+    function set(uint x) {
+        storedData = x;
+    }
+
+    function get() constant returns (uint) {
+        return storedData;
+    }
   }
 }
